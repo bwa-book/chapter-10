479ea83cf4c3cc09c4261cda3c93a12cdc828938
Mark Goody <mgoody@shopkeep.com>
2015-11-27 22:59:01 -0500

    Add the communication manager to the WatchKit extension

--- Humoji/Humoji WatchKit Extension/ExtensionDelegate.swift
+++ Humoji/Humoji WatchKit Extension/ExtensionDelegate.swift
@@ -2,6 +2,10 @@ import WatchKit
 
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
+    var communicationManager: CommunicationManager?
     
+    func applicationDidFinishLaunching() {
+        communicationManager = CommunicationManager.sharedInstance
+    }
 
 }

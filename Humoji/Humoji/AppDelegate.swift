ae2cb3b3aac9e1d4ca831911bb3811159945fcc4
Mark Goody <mgoody@shopkeep.com>
2015-11-27 22:54:11 -0500

    Add the communication manager to ViewController

--- Humoji/Humoji/AppDelegate.swift
+++ Humoji/Humoji/AppDelegate.swift
@@ -4,6 +4,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        if let rootViewController = window?.rootViewController as? ViewController {
+            rootViewController.communicationManager = CommunicationManager.sharedInstance
+        }
+        
+        return true
+    }
 
 }
 
--- Humoji/Humoji/ViewController.swift
+++ Humoji/Humoji/ViewController.swift
@@ -3,6 +3,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    
+    internal var communicationManager: CommunicationManager?
 
 }
 

fb3f031d3f5a1fb64c449a6ca25529f048d57592
Mark Goody <mgoody@shopkeep.com>
2015-11-27 22:52:05 -0500

    Set up the WCSession

--- Humoji/Inter-Device Communication/CommunicationManager.swift
+++ Humoji/Inter-Device Communication/CommunicationManager.swift
@@ -3,4 +3,28 @@ import WatchConnectivity
 
 class CommunicationManager: NSObject {
     
+    static let sharedInstance = CommunicationManager()
+    private override init() {
+        super.init()
+        setupSession()
+    }
+    
+    var session: WCSession?
+    
+}
+
+extension CommunicationManager: WCSessionDelegate {
+    
+    private func setupSession() {
+        if WCSession.isSupported() {
+            session = WCSession.defaultSession()
+            if let session = session {
+                session.delegate = self
+                session.activateSession()
+            }
+        } else {
+            print("WCSession unsupported")
+        }
+    }
+    
 }

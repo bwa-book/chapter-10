7f220ecfc9c4d36015c322b77bd46c6b582d1877
Mark Goody <mgoody@shopkeep.com>
2015-11-27 22:57:51 -0500

    Handle the received message data in ViewController

--- Humoji/Humoji/ViewController.swift
+++ Humoji/Humoji/ViewController.swift
@@ -4,7 +4,21 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
-    internal var communicationManager: CommunicationManager?
+    internal var communicationManager: CommunicationManager? {
+        didSet {
+            communicationManager?.onReceivedMessageData = { data in
+                self.dataReceived(data)
+            }
+        }
+    }
+    
+    private func dataReceived(data: NSData) {
+        dispatch_async(dispatch_get_main_queue()) {
+            if let image = UIImage(data: data) {
+                self.imageView.image = image
+            }
+        }
+    }
 
 }
 
